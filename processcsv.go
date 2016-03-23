b1d14f5721162966d895fd995935abc10c224538
Aaron Fraenkel <aaron.fraenkel@gmail.com>
2016-03-23 00:02:40 -0700

    uses mapping / added example

--- processcsv.go
+++ processcsv.go
@@ -1,3 +1,12 @@
+/*
+processcsv is a csv processing library which maps records
+according to a specified output header and mapper.
+
+TO DO:
+access columns via fields names instead of indices.
+...
+*/
+
 package main
 
 import (
@@ -7,14 +16,29 @@ import (
 	"io"
 	"os"
 	"sync"
+	"strconv"
 )
 
-type outRec struct {
+// mappedRec contains the mapped record and whether
+// or not the original record was processed.
+type mappedRec struct {
 	rec       []string
 	processed bool
 }
 
-func processLines(inpath string, outpath string, errpath string) {
+
+// pull this out this in a mapping file?
+type mapping struct {
+	inhdr, outhdr []string
+	mapper map[string]func([]string)(string, error)
+}
+	
+
+// ProcessLines parses an input csv file on disk according to the given mapper
+// and writes the mapped file to outpath, with unprocessed lines written to errpath.
+//
+// to do: should take reader/writer interfaces instead of file paths.
+func ProcessLines(inpath string, outpath string, errpath string, m mapping) {
 	infile, _ := os.Open(inpath)
 	outfile, _ := os.Create(outpath)
 	errfile, _ := os.Create(errpath)
@@ -22,12 +46,12 @@ func processLines(inpath string, outpath string, errpath string) {
 	defer errfile.Close()
 
 	jobs := make(chan []string)
-	results := make(chan outRec)
+	results := make(chan mappedRec)
 
 	wg := new(sync.WaitGroup)
 	for w := 1; w <= 3; w++ {
 		wg.Add(1)
-		go processLine(jobs, results, mapper, wg)
+		go processLine(jobs, results, m, wg)
 	}
 
 	go reader(infile, jobs)
@@ -52,7 +76,7 @@ func reader(infile *os.File, jobs chan<- []string) {
 	close(jobs)
 }
 
-func writer(outfile, errfile *os.File, results <-chan outRec) {
+func writer(outfile, errfile *os.File, results <-chan mappedRec) {
 	w := csv.NewWriter(outfile)
 	e := csv.NewWriter(errfile)
 
@@ -69,31 +93,70 @@ func writer(outfile, errfile *os.File, results <-chan outRec) {
 	e.Flush()
 }
 
-func processLine(jobs <-chan []string, results chan<- outRec, mapper func([]string) ([]string, error), wg *sync.WaitGroup) {
+
+func processLine(jobs <-chan []string, results chan<- mappedRec, m mapping, wg *sync.WaitGroup) {
 	defer wg.Done()
+	l := len(m.outhdr)
+	out := make([]string, l)
 
 	for line := range jobs {
-		mapped, err := mapper(line)
-		if err != nil {
-			results <- outRec{line, false}
-		} else {
-			results <- outRec{mapped, true}
+		var k int
+		for _, field := range m.outhdr {
+			f := m.mapper[field]
+			result, err := f(line)
+			if err != nil {
+				results <- mappedRec{line, false}
+				break
+			} else {
+				out[k] = result
+			}
+			k++
+		}
+		if k == l {
+			results <- mappedRec{out, true}
 		}
 	}
 }
 
-func mapper(record []string) ([]string, error) {
-	if record[0] == "athletics" {
-		return record[:3], nil
+
+// Example mapping -- to be hacked out into example file
+
+func isOakland(rec []string)(string, error) {
+	if rec[0] == "athletics" {
+		return "athletics", nil
 	} else {
-		return record, errors.New("")
+		return "", errors.New("")
 	}
 }
 
+func winDiff(rec []string)(string, error) {
+	w, err1 := strconv.Atoi(rec[1])
+	l, err2 := strconv.Atoi(rec[2])
+	var err error
+
+	if err1 != nil {
+		err = err1 
+	} else if err2 != nil {
+		err = err2
+	}
+			
+	return strconv.Itoa(w-l), err
+}
+
+var h1  = []string{"team","wins","losses"}
+
+var h2  = []string{"team", "win diff"}
+
+var trans = map[string]func([]string)(string, error){
+	"team": isOakland,
+	"win diff": winDiff,
+}
+
 func main() {
-	inpath := "./test2.csv"
+	m:= mapping{inhdr: h1, outhdr: h2, mapper: trans}
+	inpath := "./test.csv"
 	outpath := "./test.out.csv"
 	errpath := "./test.err.csv"
 
-	processLines(inpath, outpath, errpath)
+	ProcessLines(inpath, outpath, errpath, m)
 }
